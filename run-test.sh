aa6ec21e113dd312c4b65160dc2f83d3772eaf50
Ricardo Garcia Silva <ricardo.garcia.silva@gmail.com>
2024-03-11 18:19:22 +0000

    adding action steps

---
+++ run-test.sh
@@ -0,0 +1,15 @@
+form_args=$(for test_arg in ${INPUT_TEST_SESSION_ARGUMENTS}; do printf -- '--form %s' "${test_arg} "; done)
+
+curl_command=$(
+  printf '%s' \
+      "curl " \
+      "--user teamengine:tester " \
+      "--silent " \
+      "${form_args} " \
+      "http://localhost:8080/teamengine/rest/suites/${INPUT_TEST_SUITE_IDENTIFIER}/run"
+)
+
+test_result=$(docker exec team-engine bash -c "${curl_command}")
+
+#echo "test-result=${test_result}"
+echo "test-result=${test_result}" >> $GITHUB_OUTPUT
