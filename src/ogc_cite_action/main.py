437fa4e92528a84f2a616bd54a402dcdc00a7ee1
Ricardo Garcia Silva <ricardo@kartoza.com>
2024-07-13 00:10:52 +0100

    implement action in Python

---
+++ src/ogc_cite_action/main.py
@@ -0,0 +1,64 @@
+"""Run teamengine and parse results."""
+
+import logging
+import typing
+from pathlib import Path
+
+import click
+import httpx
+import typer
+from rich import print
+from rich import print_json
+
+from . import teamengine_runner
+
+logger = logging.getLogger(__name__)
+app = typer.Typer()
+
+
+@app.callback()
+def base_callback(
+    ctx: typer.Context,
+    debug: bool = False,
+    network_timeout: int = 20
+) -> None:
+    logging.basicConfig(level=logging.DEBUG if debug else logging.INFO)
+    ctx_obj = ctx.ensure_object(dict)
+    ctx_obj.update({
+        "debug": debug,
+        "network_timeout": network_timeout,
+    })
+    ctx_obj["debug"] = debug
+
+
+@app.command()
+def execute_test_suite(
+    ctx: typer.Context,
+    teamengine_base_url: str,
+    test_suite_identifier: str,
+    test_suite_input: typing.Annotated[
+        typing.Optional[list[click.Tuple]],
+        typer.Option(click_type=click.Tuple([str, str]))
+    ] = None,
+    persist_response: typing.Optional[Path] = None
+):
+    logger.debug(f"{locals()=}")
+    client = httpx.Client(timeout=ctx.obj["network_timeout"])
+    suite_args = {}
+    for arg_name, arg_value in (test_suite_input or []):
+        values = suite_args.setdefault(arg_name, [])
+        values.append(arg_value)
+    raw_result = teamengine_runner.execute_test_suite(
+        client,
+        teamengine_base_url.strip("/"),
+        test_suite_identifier,
+        test_suite_arguments=suite_args,
+        teamengine_username="ogctest",
+        teamengine_password="ogctest",
+    )
+    if persist_response is not None:
+        persist_response.write_text(raw_result)
+        logger.debug(f"Wrote raw response to {persist_response!r}")
+    test_summary = teamengine_runner.parse_test_results(raw_result)
+    print_json(data=test_summary)
+
---
+++ src/ogc_cite_action/teamengine_runner.py
@@ -0,0 +1,41 @@
+"""Parse test results."""
+
+import logging
+import typing
+from xml.etree import ElementTree as ET
+
+import httpx
+
+logger = logging.getLogger(__name__)
+
+
+def execute_test_suite(
+    client: httpx.Client,
+    teamengine_base_url: str,
+    test_suite_identifier: str,
+    *,
+    test_suite_arguments: typing.Optional[dict[str, str]] = None,
+    teamengine_username: str,
+    teamengine_password: str,
+) -> str:
+    response = client.get(
+        f"{teamengine_base_url}/rest/suites/{test_suite_identifier}/run",
+        params=test_suite_arguments,
+        auth=(teamengine_username, teamengine_password),
+        headers={
+            "Accept": "application/xml",
+        }
+    )
+    try:
+        response.raise_for_status()
+    except httpx.HTTPError:
+        logger.exception(msg="Could not execute test suite")
+        logger.debug(response.content)
+    else:
+        return response.text
+
+
+def parse_test_results(raw_results: str) -> dict:
+    root = ET.fromstring(raw_results)
+    summary = root.attrib
+    return summary
